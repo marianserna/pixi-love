0ded8ed4c18e61ba0f784eb7d04fc4a8587acf37
Marian Serna <marianhalliday@gmail.com>
2017-11-05 14:23:33 -0500

    blobs & random stuff

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
-
 import * as PIXI from 'pixi.js'
 
+var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0xFF6600});
+
+document.body.appendChild(app.view);
+document.body.style.margin = 0;
+
+var sprite = PIXI.Sprite.from('./assets/Can-9.png');
+
+var target = new PIXI.Point();
+app.stage.interactive = true;
+
+app.stage.mousemove = (e) => {
+  target.x = e.data.global.x;
+  target.y = e.data.global.y;
+}
+
+var container = new PIXI.Container();
+var cmFilter = new PIXI.filters.ColorMatrixFilter();
+container.filters = [cmFilter];
+
+app.stage.addChild(container);
+var blobs = [];
+
+for (var i = 0; i < 50; i++) {
+  var blob = PIXI.Sprite.from('./assets/blob.png');
+  blobs.push(blob);
+
+  blob.anchor.set(0.5);
+  blob.count = Math.random() * Math.PI * 2;
+  container.addChild(blob);
+}
+
+var tick = 0;
+
+app.ticker.add((dt) => {
+  tick += 0.01;
+  // container.position.x = target.x;
+  // container.position.y = target.y;
+  // sprite.rotation += 0.01
+  for (var i = 0; i < blobs.length; i++) {
+    var blob = blobs[i];
+    blob.count += 0.1;
+
+    blob.scale.set(Math.sin(blob.count * 0.5));
+    blob.alpha = 0.9;
+
+    if(blob.count > Math.PI * 2) {
+      blob.count -= Math.PI * 2;
+
+      var angle = Math.random() * Math.PI * 2;
+      var distance = 100 + Math.random() * 100;
+
+      blob.x = target.x + Math.sin(angle) * distance;
+      blob.y = target.y + Math.cos(angle) * distance;
+    }
+  }
+});
+
+window.addEventListener('resize', () => {
+  app.renderer.resize(window.innerWidth, window.innerHeight)
+});
---
+++ random.js
@@ -0,0 +1,59 @@
+//
+// app.stage.addChild(sprite);
+// sprite.interactive = true;
+// sprite.accessible = true;
+// sprite.buttonMode = true;
+// sprite.mouseover = () => {
+//   sprite.alpha = 0.5;
+//   sprite.scale.set(5);
+// }
+//
+// sprite.click = () => {
+//   alert("Hello");
+// }
+//
+// sprite.position.x = window.innerWidth / 2;
+// sprite.position.y = window.innerHeight / 2;
+//
+//
+// sprite.scale.x = 3;
+// sprite.scale.y = 3;
+// // sprite.alpha = 0.8;
+//
+// sprite.anchor.x = 0.5;
+// sprite.anchor.y = 0.5;
+// var container = new PIXI.Container();
+// // app.stage.addChild(container);
+//
+// // var sprites = [];
+// //
+// // for (var i = 0; i < 200; i++) {
+// //   var sprite = PIXI.Sprite.from('./assets/Can-8.png');
+// //   sprite.position.x = Math.random() * window.innerWidth;
+// //   sprite.position.y = Math.random() * window.innerHeight;
+// //   sprite.anchor.set(0.5);
+// //
+// //   container.addChild(sprite);
+// //   sprites.push(sprite);
+// // }
+// //
+// // // Draw circle
+// // var graphics = new PIXI.Graphics()
+// // // .beginFill(0xFF3300)
+// // // .drawRect(0, 0, 200, 100)
+// // // .beginFill(0xCCCCCC)
+// // // .drawCircle(200, 0, 40)
+// // .lineStyle(4, 0x0000FF)
+// // .moveTo(10, 10)
+// // .lineTo( 100, 100)
+// //
+// // for (var i = 0; i < 30; i++) {
+// //   graphics.lineTo(Math.random() * 300, Math.random() * 300)
+// // }
+// //
+// // graphics.position.x = window.innerWidth / 2;
+// // graphics.position.y = window.innerHeight / 2;
+// //
+// // app.stage.addChild(graphics);
+// // // app.stage.removeChild(graphics);
+// //
